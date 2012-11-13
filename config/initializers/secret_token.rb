240ac146a5e9538816f12abe5f754689ab536de2
Igor Trofimov <igortr333@gmail.com>
2012-11-13 21:56:22 +0400

    fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wallstr::Application.config.secret_token = 'd15659f44c05d17e508a294ca0f36f7f66eea61426a87f0ca7838898eb89e2fdee97b3df132e4f31c3fdae9532bf65720b2353d478ba3e678cc74299d52f8c9d'
